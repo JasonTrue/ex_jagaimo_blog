516dd7c1116f3f00058fef65601dffbbf4304415
Jason Truesdell <jason@yuzuten.com>
2021-06-14 10:18:48 +0900

    Add Tag ranking function
    Calculates tags and orders them
    
    There's a small template change to display tags associated with a
    particular post.
    
    Tag cloud just needs to leverage the data in the Tags context function
    list_ranked_tags_by_blog/1
    
    I might change the shape of this data later

---
+++ lib/ex_jagaimo_blog/tags.ex
@@ -0,0 +1,16 @@
+defmodule ExJagaimoBlog.Tags do
+  @moduledoc """
+  The Tags context.
+  """
+
+  import Ecto.Query, warn: false
+  alias ExJagaimoBlog.Blogs.{Blog, Post}
+
+  def list_ranked_tags_by_blog(%Blog{id: blog_id}) do
+    from post in Post, where: post.blog_id == ^blog_id,
+    inner_join: tag in assoc(post, :tags),
+    group_by: [tag.id, tag.name],
+    order_by: [desc: count(post.id)],
+    select: {tag.id, tag.name, count(post.id)}
+  end
+end
