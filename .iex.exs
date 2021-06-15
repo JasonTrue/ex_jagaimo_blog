a5fbe3b5195923ae58233b2560e85048535edece
Jason Truesdell <jason@yuzuten.com>
2021-06-16 00:19:17 +0900

    Add default aliases to .iex.exs

---
+++ .iex.exs
@@ -0,0 +1,4 @@
+alias ExJagaimoBlog.{Repo,Blogs,Tags,Repo,Accounts}
+alias ExJagaimoBlog.Blogs.{Post, Blog}
+alias ExJagaimoBlog.Tags.{Tag, PostTagging}
+alias ExJagaimoBlogWeb.{Controllers,Channels,Helpers,Views}
